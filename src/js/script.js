b8449a393bd72f2d790d32aecfdfe05c1f11a2d4
Leonardo <leonardorscarpitta@gmail.com>
2024-05-03 14:26:43 -0300

    Estilizando página inicial

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 
 
 
-let imagens= ['src/assets/bike1.webp', 'src/assets/bike2.jpg', 'src/assets/bike3.jpg','src/assets/bike4.png','src/assets/bike5.webp'];
+let imagens= ['src/assets/bike1.webp', 'src/assets/bike2.png', 'src/assets/bike3.webp','src/assets/bike4.png','src/assets/bike5.webp'];
 let index=0;
 let time = 3000;
 
