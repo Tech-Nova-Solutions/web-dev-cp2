bbd4a3adec3be274148deb446e9a562232dd8e69
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-05-03 12:04:17 -0300

    Criando alert bem-vindo e estruturando pagina de login

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
+//alert("Seja Bem-Vindo(a)");
+
+
+
 let imagens= ['src/assets/bike1.webp', 'src/assets/bike2.jpg', 'src/assets/bike3.jpg','src/assets/bike4.png','src/assets/bike5.webp'];
 let index=0;
 let time = 3000;
@@ -43,4 +47,5 @@ function mudarFundo() {
     
     indexCor = (indexCor + 1) % cores_fundo.length;
 }
+
 
